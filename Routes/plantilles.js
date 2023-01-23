f5008b73c73ecaf73ce5a89a8453929a6c4504e5
victorf98 <vflores111@boscdelacoma.cat>
2023-01-23 19:49:59 +0100

    fet get i creates dia, guardia i plantilles

--- Routes/plantilles.js
+++ Routes/plantilles.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { body } = require('express-validator');
-const plantillaController = require('../Controllers/plantilla.js');
+const plantillaController = require('../Controllers/plantilles.js');
 
-// router.get('/getPlantilles', plantillaController.getPlantilles);
-// router.post('/createPlantilla', plantillaController.createPlantilla);
+router.get('/', plantillaController.getPlantilles);
+router.post('/createPlantilla', plantillaController.createPlantilla);
 
 module.exports = router
