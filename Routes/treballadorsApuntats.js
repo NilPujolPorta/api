9a131bdd1e7cd81e9c15aa759c871d477fb700f8
Nil Pujol <nilpujolporta@gmail.com>
2023-01-23 19:59:00 +0100

    treballadorsApuntats + dat-and-time install

---
+++ Routes/treballadorsApuntats.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+
+const { body } = require('express-validator');
+const treballadorsApuntatsController = require('../Controllers/treballadorsApuntats.js')
+
+router.get('/', treballadorsApuntatsController.getTrebelladorsApuntats);
+router.get('/getIDGuardiesByTreballador', treballadorsApuntatsController.getIDGuardiesByTreballador)
+router.get('/getIDTreballadorsByIdGuardia', treballadorsApuntatsController.getIDTreballadorsByIdGuardia)
+router.post('/apuntarTreballador', treballadorsApuntatsController.apuntarTreballador);
+
+module.exports = router
+
+
