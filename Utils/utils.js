9862bda85ff07c21f6004cd4babe9af2d5421f4b
Nil Pujol <nilpujolporta@gmail.com>
2023-01-31 16:38:50 +0100

    afegir rolFromToken

--- Utils/utils.js
+++ Utils/utils.js
@@ -1,3 +1,6 @@
-function rolFromToken(token) {
+const { decode } = require("jsonwebtoken");
 
+const permisosBasics = ["user"];
+function rolFromToken(token) {
+    return JSON.parse(jwt_decode(token)).rol;
 }
