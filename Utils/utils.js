286eb12f81fe63fe6763a15e87e125ca976fd44a
Nil Pujol <nilpujolporta@gmail.com>
2023-02-02 17:15:32 +0100

    validation prep

--- Utils/utils.js
+++ Utils/utils.js
@@ -1,8 +1,28 @@
-const { decode } = require("jsonwebtoken");
 import jwt_decode from "jwt-decode";
-import { getTreballadorByUsername } from "../Controllers/treballador";
+const treballador = require("../Controllers/treballador.js")
+const Token = require('../Model/Implementations/Token/token.js')
+const jwt = require("jsonwebtoken")
 
 const permisosBasics = ["user"];
 function rolFromToken(token) {
-    return getTreballadorByUsername(jwt_decode(token).user.usuari).rol;
+    return treballador.getTreballadorByUsername(jwt_decode(token).user.usuari).rol;
+}
+const validateTokenG = (async (req, res, next) => {
+    try {
+        const token = new Token();
+        const accessToken = req.headers["authorization"].split(" ")[1];
+        if (accessToken == null) res.sendStatus(400).send("Token not present")
+        jwt.verify(accessToken, token.secret, (err, user) => {
+            if (err) res.status(403).send("Token invalid")
+            else next();
+        })
+    } catch (error) {
+        res.status(400).json({ missatge: error })
+    }
+})
+
+module.exports = {
+    validateTokenG,
+    permisosBasics,
+    rolFromToken
 }
