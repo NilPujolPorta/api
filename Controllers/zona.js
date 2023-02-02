7f70d29f2c831802d29a6cb5d4f245dfaced71d7
Nil Pujol <nilpujolporta@gmail.com>
2023-02-02 16:58:00 +0100

    try catches 2a part

--- Controllers/zona.js
+++ Controllers/zona.js
@@ -2,25 +2,34 @@ const db = require('../Utils/database');
 const jwt = require("jsonwebtoken");
 
 const createZona = (async (req, res) => {
-    const nom = req.body.nom;
-    const usuariMOD = req.body.usuariMOD;
-    if (nom == getZonesReturn()[0]) {
-        res.status(400).json({ missatge: 'Aquesta zona ja existeix' });
-    }
-    db.execute(
-        "INSERT INTO Zona (nom, usuariMOD) VALUES (?, ?)",
-        [nom, usuariMOD]
-    )
+    try {
+        const nom = req.body.nom;
+        const usuariMOD = req.body.usuariMOD;
+        if (nom == getZonesReturn()[0]) {
+            res.status(400).json({ missatge: 'Aquesta zona ja existeix' });
+        }
+        db.execute(
+            "INSERT INTO Zona (nom, usuariMOD) VALUES (?, ?)",
+            [nom, usuariMOD]
+        )
 
-    res.status(201).json({ missatge: 'Zona registrada correctament' });
+        res.status(201).json({ missatge: 'Zona registrada correctament' });
+    } catch (error) {
+        res.status(400).json({ missatge: error })
+    }
+
 })
 
 const getZones = (async (req, res) => {
-    let resposta = [];
-    await db.execute(
-        'SELECT * FROM Zona'
-    ).then(result => resposta = result[0]);
-    res.status(200).json(resposta);
+    try {
+        let resposta = [];
+        await db.execute(
+            'SELECT * FROM Zona'
+        ).then(result => resposta = result[0]);
+        res.status(200).json(resposta);
+    } catch (error) {
+        res.status(400).json({ missatge: error })
+    }
 })
 async function getZonesReturn() {
     let resposta = [];
